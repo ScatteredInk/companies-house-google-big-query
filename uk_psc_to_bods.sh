4cd4a5537474ffb57d727867a248816f810f1ca5
Jack Lord <jack.lord@opendataservices.coop>
2018-02-14 16:58:51 +0000

    separate entity types in jq script

--- uk_psc_to_bods.sh
+++ uk_psc_to_bods.sh
@@ -3,18 +3,26 @@ exec jq -s . |
 jq '[.[]]' |
 jq 'group_by(.data.links.self) |
 map(
-{entity: {identifiers: [{scheme: "GB-COH", id: .[0].company_number }]}, 
-interestedParty: 
-    {
-        name: .[].data.name,
-        type: map(if .data.kind == "individual-person-with-significant-control"
-                  	then "naturalPerson"
-                  elif .data.kind == "corporate-entity-person-with-significant-control"
-                  	then "registeredEntity"
-                  elif .data.kind == "legal-person-person-with-significant-control"  
-        		  then "legalEntity" else null end),
-        nationalities: [.[].data.nationality]
-    },
+{entity: {identifiers: [{scheme: "GB-COH", id: .[0].company_number }]},
+          interestedParty: map(
+            if .data.kind == "individual-person-with-significant-control" then 
+                {
+                  name: .data.name,
+                  type: "naturalPerson",
+                  nationalities: [.data.nationality]
+                 } 
+           elif .data.kind == "corporate-entity-person-with-significant-control" then
+                {
+                    name: .data.name,
+                    type: "registeredEntity"
+                }
+            elif .data.kind == "legal-person-person-with-significant-control" then
+                {
+                    name: .data.name,
+                    type: "legalEntity"
+                }
+            else
+                null end ),
 interests: map( {  details: .data.natures_of_control[] } )})' | 
 jq 'group_by(.entity.identifiers[].id) | map(
 {statementGroup: {
